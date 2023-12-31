704ade75b2204a9822b7785d4ac8759c273d9316
mimzy <tzachi@diysimchas.com>
2023-12-30 22:42:19 -0500

    refactor after fixing exit on bad local command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,62 @@
 use std::io;
 use std::io::Write; // to use flush
 use std::process::Command; //to run commands on the system
+use std::process::Output; //to run commands on the system
+
+
+fn main() {
+    loop {
+        print!("oxygen> "); // Print the prompt
+        io::stdout().flush().unwrap(); // Ensure the prompt is displayed immediately
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap(); // Read user input
+        let input = input.trim(); // Trim newline characters
+
+        // check if the input is a slash command
+        let first_char = input.chars().next(); // Get the first character
+        match first_char {
+            Some('\\') => {
+                handle_slash_command(input);
+                continue;
+            }, // Handle slash commands
+            Some(_) => (), // move on
+            None => continue, // no input so next iteration
+        }
+
+        // handle all other commands
+        match input {
+            "hi" => println!("Hello, oxidized world!"), // Greet the world!
+            "clear" => print!("{}[2J{}[H", 27 as char, 27 as char), // Clear the terminal and move cursor to top-left  "clear" => print!("{}[2J", 27 as char), // Clear the terminal,
+            "exit" => break, // Exit the loop if the input is "exit"
+            "" => continue, // Ignore empty input and show the prompt again
+            _ => handle_command(input)
+        }
+    }
+}
+
+fn handle_slash_command(command: &str) {
+    // Remove the slash from the command
+    let command = command.trim_start_matches('\\');
+    let first_word = command.split_whitespace().next().unwrap();
+
+    // Check if the command is a local command
+    match first_word {
+        "run" => run_local_command(command.trim_start_matches("run ")),
+        _ => println!("Slash Command not found: {}", command)
+    }
+}
+
+fn handle_command(command: &str) {
+
+    let mut command = command.split_whitespace();
+    let command_name = command.next().unwrap();
+
+    match command_name {
+        // "run" => run_local_command(command.as_str()),
+        _ => println!("Command not found: {}", command_name)
+    }
+}
 
 fn run_local_command(command: &str) {
     // Confirm with user if it's okay to run the command locally
@@ -26,10 +82,22 @@ fn run_local_command(command: &str) {
             let output = Command::new(command_name)
                 .args(args)
                 .output()
-                .expect("Failed to run command");
+                .unwrap_or_else(|error| {
+                    // Print the error message
+                    eprintln!("Failed to run command: {}", error);
+                    
+                    // Create an empty output
+                    Output {
+                        status: Default::default(),
+                        stdout: Default::default(),
+                        stderr: Default::default(),
+                    }
+                });
 
             // Print the output
             println!("{}", String::from_utf8_lossy(&output.stdout));
+            // Print the error message
+            eprintln!("{}", String::from_utf8_lossy(&output.stderr));
         }
         "n" | "esc" => {
             println!("Command execution cancelled");
@@ -38,58 +106,6 @@ fn run_local_command(command: &str) {
         _ => {
             println!("Invalid input. Command execution cancelled");
             return;
-        }
-    }
-}
-
-fn handle_slash_command(command: &str) {
-    // Remove the slash from the command
-    let command = command.trim_start_matches('\\');
-    let first_word = command.split_whitespace().next().unwrap();
-
-    // Check if the command is a local command
-    match first_word {
-        "run" => run_local_command(command.trim_start_matches("run ")),
-        _ => println!("Slash Command not found: {}", command)
-    }
-}
-
-fn main() {
-    loop {
-        print!("oxygen> "); // Print the prompt
-        io::stdout().flush().unwrap(); // Ensure the prompt is displayed immediately
-
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap(); // Read user input
-        let input = input.trim(); // Trim newline characters
-
-        // check if the input is a slash command
-        let first_char = input.chars().next(); // Get the first character
-        match first_char {
-            Some('\\') => {
-                handle_slash_command(input);
-                continue;
-            }, // Handle slash commands
-            Some(_) => (), // move on
-            None => continue, // no input so next iteration
-        }
-
-        /*
-        // look for the first word in the input
-        let first_word = sentence.split_whitespace().next();
-        match first_word {
-            Some("run") => run_local_command(sentence),
-            Some(_) => (), // move on
-            None => continue, // no input so next iteration
-        }
-        */
-        
-        match input {
-            "hi" => println!("Hello, oxidized world!"), // Greet the world!
-            "clear" => print!("{}[2J{}[H", 27 as char, 27 as char), // Clear the terminal and move cursor to top-left  "clear" => print!("{}[2J", 27 as char), // Clear the terminal,
-            "exit" => break, // Exit the loop if the input is "exit"
-            "" => continue, // Ignore empty input and show the prompt again
-            _ => println!("command not found: {}", input),
         }
     }
 }
