3421ad0c6511126d11da4fc4b6a267fbb58cae7e
mimzy <tzachi@diysimchas.com>
2024-01-03 21:51:27 -0500

    tested openai refactor

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
+use core::llm::openai;
+use tokio::runtime::Runtime;
 
 fn main() {
-    println!("Hello, world!");
+    let res = Runtime::new().unwrap().block_on(
+        openai::prompt(
+            String::from("MARCO!"),
+            openai::Conversation { messages: vec![] }    
+        )
+    );
+
+    match res {
+        Ok(msg) => println!("Response: {}", msg.content),
+        Err(e) => println!("Error: {}", e),
+    }
 }
