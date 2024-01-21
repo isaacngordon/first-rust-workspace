3c5d013162d851782cf2bb1bfa1ce533fba118bf
isaacngordon <45136480+isaacngordon@users.noreply.github.com>
2024-01-21 10:21:18 -0500

    auto many test cases

--- src/crates/misc/conway/benches/algo_comparison.rs
+++ src/crates/misc/conway/benches/algo_comparison.rs
@@ -2,21 +2,41 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use conway::game_of_life::slice::Slice;
 
-fn benchmark_next_generation(c: &mut Criterion) {
+
+/// Benchmark the next_generation method for a given size and number of steps using each algorithm
+fn benchmark_next_generation(c: &mut Criterion, size: usize, steps: usize) {
     let mut group = c.benchmark_group("Slice next_generation");
-    let mut slice = Slice::new(256);
+    let mut slice = Slice::new(size);
     slice.randomize();
 
-    group.bench_function("naive", |b| {
-        b.iter(|| slice.next_generation_naive())
+    group.bench_function(format!("naive_{}_{}", size, steps), |b| {
+        let mut slice_clone = slice.clone();
+        b.iter(|| 
+            for _ in 0..steps {
+                slice_clone.next_generation_naive()
+            }
+        )
     });
-    group.bench_function("naive_optimized", |b| {
-        b.iter(|| slice.next_generation_naive_optimized())
+    group.bench_function(format!("naive_optimized_{}_{}", size, steps), |b| {
+        let mut slice_clone = slice.clone();
+        b.iter(|| 
+            for _ in 0..steps {
+                slice_clone.next_generation_naive_optimized()
+            }
+        )
     });
     group.finish();
 }
 
-criterion_group!(benches, benchmark_next_generation);
+/// Benchmark each next_generation algorithm for a single iteration for 11 different sizes
+fn benchmark_next_generation_single_iteration(c: &mut Criterion) {
+    let test_cases = vec![3, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048];
+    test_cases.iter().for_each(|size| {
+        benchmark_next_generation(c, *size, 1);
+    });
+}
+
+criterion_group!(benches, benchmark_next_generation_single_iteration);
 criterion_main!(benches);
 
 
